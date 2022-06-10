80c4eccccaa21b5bd10f6d8506310d2381856e74
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-06-10 12:08:43 +0200

    order-details added

--- src/app/order-detail/order-detail.component.ts
+++ src/app/order-detail/order-detail.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Order } from 'src/models/order.class';
 
 @Component({
   selector: 'app-order-detail',
   templateUrl: './order-detail.component.html',
-  styleUrls: ['./order-detail.component.scss']
+  styleUrls: ['./order-detail.component.scss'],
 })
 export class OrderDetailComponent implements OnInit {
-
-  constructor() { }
+  orderId = '';
+  order: Order = new Order();
+  constructor(private firestore: AngularFirestore) {}
 
   ngOnInit(): void {
+    this.getOrder();
   }
 
+  getOrder() {
+    this.firestore
+      .collection('orders')
+      .doc(this.orderId)
+      .valueChanges()
+      .subscribe((order: any) => {
+        this.order = new Order(order);
+        console.log('Retrieved order', this.order);
+      });
+  }
 }
