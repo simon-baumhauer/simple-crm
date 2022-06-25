8bbedbfa96fc7e88db2a04d5505a883aa710ccfb
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-06-25 09:38:15 +0200

    price of all Order added to Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Order } from 'src/models/order.class';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  order = new Order();
+  allOrders = [];
+  finalSum = 0;
+  constructor(private firestore: AngularFirestore) {}
 
   ngOnInit(): void {
+    this.firestore
+      .collection('orders')
+      .valueChanges({ idField: 'customIdName' })
+      .subscribe((changes: any) => {
+        console.log('Receive changes from DB', changes);
+        this.allOrders = changes;
+        console.log('after', this.allOrders);
+        this.sumUp();
+      });
+    console.log('all Orders in Dashboard:', this.allOrders);
   }
 
+  sumUp() {
+    for (let i = 0; i < this.allOrders.length; i++) {
+      const element = this.allOrders[i];
+      this.finalSum = this.finalSum + element.price;
+      console.log(this.finalSum);
+    }
+  }
 }
